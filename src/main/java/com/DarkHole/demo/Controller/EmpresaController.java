f553f6d47250ec9c0d96764bde863a91ef0dc1aa
Akir4 <dkaua0955@gmail.com>
2026-05-12 23:08:49 -0300

    Adicionando o controller

--- src/main/java/com/DarkHole/demo/Controller/EmpresaController.java
+++ src/main/java/com/DarkHole/demo/Controller/EmpresaController.java
@@ -1,9 +1,20 @@
 package com.darkhole.demo.Controller;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.darkhole.demo.Service.EmpresaService;
+import com.darkhole.demo.dto.Request.EmpresaRequestDTO;
+import com.darkhole.demo.dto.Response.EmpresaResponseDTO;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
 
 
 @RestController
@@ -11,4 +22,17 @@ import com.darkhole.demo.Service.EmpresaService;
 public class EmpresaController {
     
     private EmpresaService service;
+
+
+    @PostMapping
+    public ResponseEntity<EmpresaResponseDTO> cadastrarEmpresa(@RequestBody @Valid EmpresaRequestDTO dto) {
+        EmpresaResponseDTO response = service.salvarEmpresa(dto);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity <EmpresaResponseDTO> buscarPorId(@PathVariable Long id){
+        EmpresaResponseDTO response = service.buscarPorId(id);
+        return ResponseEntity.ok(response);
+    }    
 }
